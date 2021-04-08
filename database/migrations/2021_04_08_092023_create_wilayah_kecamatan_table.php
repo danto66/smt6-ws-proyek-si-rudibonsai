59f523377001e00b9df62bd22ba4332632014da0
Danang ardianto <ardhyanto6629@gmail.com>
2021-04-08 17:01:22 +0700

    tambah tabel alamat, seed tabel alamat, file sql

---
+++ database/migrations/2021_04_08_092023_create_wilayah_kecamatan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWilayahKecamatanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wilayah_kecamatan', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->string('kabupaten_id');
+            $table->string('nama');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wilayah_kecamatan');
+    }
+}
