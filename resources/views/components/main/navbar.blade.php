5603e86fbc186c18439e22b5b4a7b3e9e9e81ca8
Danang ardianto <ardhyanto6629@gmail.com>
2021-04-18 16:19:04 +0700

    update navbar

--- resources/views/components/main/navbar.blade.php
+++ resources/views/components/main/navbar.blade.php
@@ -26,49 +26,101 @@
 
                 <div class="hidden sm:absolute sm:flex sm:justify-center sm:w-full">
                     <div class="flex space-x-4">
-                        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="#" class="px-3 py-2 border-b-2 border-green-500 text-sm font-medium"
+                        <a href="/" class="px-3 py-2 border-b-2 border-green-500 text-sm font-medium"
                             aria-current="page">Home</a>
 
-                        <a href="#" class="text-gray-500 hover:text-black px-3 py-2  text-sm font-medium">Produk</a>
+                        <a href="/products"
+                            class="text-gray-500 hover:text-black px-3 py-2 text-sm font-medium">Produk</a>
 
-                        <a href="#" class="text-gray-500 hover:text-black px-3 py-2 text-sm font-medium">Info</a>
+                        <div class="relative">
+                            <button x-on:click="{dropdownInfo = !dropdownInfo}"
+                                class="text-gray-500 hover:text-black px-3 py-2 text-sm font-medium">Info
+                                <i class="fas fa-caret-down"></i>
+                            </button>
+                            <div x-show="dropdownInfo" x-on:click.away="{dropdownInfo = !dropdownInfo}"
+                                class="top-12 py-2 w-48 right-0 rounded absolute shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <a href=""
+                                    class="text-gray-500 hover:bg-gray-200 hover:text-black block py-2 px-4 text-sm font-medium rounded-t">Tentang
+                                    Kami</a>
+                                <a href=""
+                                    class="text-gray-500 hover:bg-gray-200 hover:text-black block py-2 px-4 text-sm font-medium ">Kontak</a>
+                                <a href=""
+                                    class="text-gray-500 hover:bg-gray-200 hover:text-black block py-2 px-4 text-sm font-medium ">Pembayaran</a>
+                                <a href=""
+                                    class="text-gray-500 hover:bg-gray-200 hover:text-black block py-2 px-4 text-sm font-medium rounded-b">Pengiriman</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
 
             @auth
                 <div class="absolute inset-y-0 right-0 z-30 flex items-center">
                     <!-- Profile dropdown -->
-                    <div>
-                        <button x-on:click="open('dropdown')" type="button"
-                            class="bg-green-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-800 focus:ring-white"
-                            id="user-menu" aria-expanded="false" aria-haspopup="true">
+                    <div class="relative flex justify-between">
+                        <a class="hidden sm:flex justify-center text-sm rounded-xl px-3 py-1 bg-green-300" href="">
+                            <i class="my-auto fas fa-shopping-cart mr-2"></i>
+                            <span class="text-sm my-auto font-semibold">36</span>
+                        </a>
+
+                        <button x-on:click="open('dropdown')" type="button" class="ml-2 text-3xl flex" id="user-menu"
+                            aria-expanded="false" aria-haspopup="true">
+                            <span
+                                class="sm:hidden absolute font-semibold -top-2 right-7 bg-green-300 p-1 rounded-lg text-xs">36</span>
                             <span class="sr-only">Open user menu</span>
-                            <img class="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt="">
+                            @if (auth()->user()->userProfile->profile_picture == 'default')
+                                <i class="fas fa-user-circle my-auto"></i>
+                            @else
+                                <img class="h-8 w-8 rounded-full"
+                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    alt="">
+                            @endif
                         </button>
                     </div>
                 </div>
                 <div x-show="isOpen('dropdown')" x-on:click.away="close"
-                    class="top-14 w-full sm:w-1/4 right-0 absolute rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    class="top-16 w-full sm:w-48 right-0 absolute rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                     role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your
-                        Profile</a>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign
-                        out</a>
+                    <div href="#" class="border-b-2 border-gray-100 block px-4 py-2 text-sm font-semibold" role="menuitem">
+                        {{ auth()->user()->userProfile->fullname }} </div>
+                    <a href="#" class="font-semibold sm:hidden block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        role="menuitem">
+                        <i class="mr-2 fas fa-shopping-cart"></i>
+                        <span>
+                            Keranjang (32)
+                        </span>
+                    </a>
+                    <a href="#" class="font-semibold block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        role="menuitem">
+                        <i class="mr-2 fas fa-user"></i>
+                        Profil
+                    </a>
+                    <a href="#" class="font-semibold block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        role="menuitem">
+                        <i class="mr-2 fas fa-receipt"></i>
+                        Transaksi
+                    </a>
+
+                    <!-- Authentication -->
+                    <div class="border-gray-100 border-t-2 block hover:bg-gray-100">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <button class="w-full px-4 py-2 text-left font-semibold text-sm text-gray-700">
+                                <i class="mr-2 fas fa-sign-out-alt"></i>
+                                Keluar
+                            </button>
+                        </form>
+                    </div>
+
                 </div>
             @endauth
             @guest
                 <div class="z-30 hidden sm:inline">
-                    <a href="" class="mr-2 font-semibold hover:text-green-500 text-gray-800">Masuk</a>
-                    <x-button.btn-link href=""
-                        class="bg-green-500 text-white hover:bg-white hover:text-green-500 hover:ring-1 hover:ring-green-500">
+                    <a href="/login" class="btn hover:underline text-green-500">Masuk</a>
+                    <a href="/register" class="btn btn-outline-green hover-green">
                         Daftar
-                    </x-button.btn-link>
+                    </a>
                 </div>
             @endguest
 
@@ -79,29 +131,35 @@
     <div x-show="isOpen('menu')" x-on:click.away="close" class=" sm:hidden" id="mobile-menu">
         <div class="px-2 pt-2 pb-4 space-y-1">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="#" class="bg-gray-200 block px-3 py-2 rounded-md text-base font-medium"
+            <a href="#" class="rounded bg-gray-200 block px-3 py-2 text-base font-medium border-l-2 border-green-500"
                 aria-current="page">Home</a>
 
-            <a href="#"
-                class="text-gray-500 hover:bg-gray-200 hover:text-black block px-3 py-2 rounded-md text-base font-medium">Produk</a>
+            <a href="/products"
+                class="rounded text-gray-500 hover:bg-gray-200 hover:text-black block px-3 py-2 text-base font-medium">Produk</a>
 
-            <button x-on:click="{dropdownInfo = !dropdownInfo}"
-                class="text-gray-500 hover:bg-gray-200 hover:text-black block px-3 py-2 rounded-md text-base font-medium w-full text-left">Info</button>
-            <div x-show="dropdownInfo" class=" p-2 rounded border-2">
-                <a href=""
-                    class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 rounded-md text-base font-medium">Tentang
-                    Kami</a>
-                <a href=""
-                    class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 rounded-md text-base font-medium">Kontak</a>
-                <a href=""
-                    class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 rounded-md text-base font-medium">Pembayaran</a>
-                <a href=""
-                    class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 rounded-md text-base font-medium">Pengiriman</a>
+            <div class="rounded border-2">
+                <button x-on:click="{dropdownInfo = !dropdownInfo}"
+                    class="text-gray-500 hover:bg-gray-200 hover:text-black block px-3 py-2 text-base font-medium w-full text-left">Info
+                    <i class="fas fa-caret-down"></i></button>
+
+                <div x-show="dropdownInfo" class="pb-2 px-3">
+                    <a href=""
+                        class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 text-base font-medium">Tentang
+                        Kami</a>
+                    <a href=""
+                        class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 text-base font-medium">Kontak</a>
+                    <a href=""
+                        class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 text-base font-medium">Pembayaran</a>
+                    <a href=""
+                        class="text-gray-500 hover:bg-gray-200 hover:text-black block p-2 text-base font-medium">Pengiriman</a>
+                </div>
             </div>
+
             @guest
                 <div class="border-t-2 pt-4 flex flex-col sm:hidden">
-                    <a href="" class="py-2 text-center font-medium text-green-500 ring-1 ring-green-500 rounded">Masuk</a>
-                    <a href=""
+                    <a href="/login"
+                        class="py-2 text-center font-medium text-green-500 ring-1 ring-green-500 rounded">Masuk</a>
+                    <a href="/register"
                         class="mt-2 py-2 text-center font-medium rounded bg-green-500 text-white hover:bg-white hover:text-green-500 hover:ring-1 hover:ring-green-500">
                         Daftar
                     </a>
