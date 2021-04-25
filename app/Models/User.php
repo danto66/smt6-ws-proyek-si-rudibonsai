393d47736faae5c8394a794407443ca36ea1cd0a
Danang ardianto <ardhyanto6629@gmail.com>
2021-04-25 16:51:02 +0700

    add model : admin profile, cart, product, role; update : user, user profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Cart;
+use App\Models\UserProfile;
+use App\Models\AdminProfile;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -43,5 +46,20 @@ class User extends Authenticatable implements MustVerifyEmail
     public function userProfile()
     {
         return $this->hasOne(UserProfile::class);
+    }
+
+    public function adminProfile()
+    {
+        return $this->hasOne(AdminProfile::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
     }
 }
--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Alamat\Kabupaten;
+use App\Models\Alamat\Kecamatan;
+use App\Models\Alamat\Provinsi;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserProfile extends Model
 {
@@ -24,5 +28,20 @@ class UserProfile extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function provinsi()
+    {
+        return $this->belongsTo(Provinsi::class);
+    }
+
+    public function kabupaten()
+    {
+        return $this->belongsTo(Kabupaten::class);
+    }
+
+    public function kecamatan()
+    {
+        return $this->belongsTo(Kecamatan::class);
     }
 }
