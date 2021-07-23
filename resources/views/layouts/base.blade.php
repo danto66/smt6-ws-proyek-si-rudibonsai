7c597decfc8a76ec9b61c223438eb81428af4971
Anggito Suryo Baskoro <e41191560@student.polije.ac.id>
2021-07-23 10:18:51 +0700

    logo title

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Rudibonsai - @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('img/logo/bonsai.svg') }}">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
