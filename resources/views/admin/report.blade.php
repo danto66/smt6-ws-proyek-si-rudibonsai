7aa1e7d373e26c1f934503e0ed8372a101c84197
Danang Ardianto <ardhyanto6629@gmail.com>
2022-06-27 11:30:12 +0700

    update report

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -37,11 +37,18 @@
 
                     <th
                         class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        Produk</th>
+                        Pelanggan</th>
 
                     <th
                         class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        Alamat</th>
+                        Total Harga Produk</th>
+
+                    <th
+                        class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        Ongkos Kirim</th>
+                    <th
+                        class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        Total Bayar</th>
 
                 </tr>
             </thead>
