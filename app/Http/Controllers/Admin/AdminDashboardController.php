7ccd78c476a2500daaaecead6b0e6e1e7a3b6d9f
Danang Ardianto <ardhyanto6629@gmail.com>
2022-06-27 11:30:01 +0700

    update dashboard

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -14,14 +14,11 @@ class AdminDashboardController extends Controller
 {
     public function index()
     {
-        $admin = User::where('role_id', 3)->paginate(10);
-        // $userProfile = UserProfile::where('user_id', auth()->User()->id)->first();
-
         $countuser = User::where('role_id', 3)->count();
         $countorder = Order::all()->count();
         $countdone = Order::where('status', 'Selesai')->count();
+        $income = Order::select('grand_total_amount')->get()->sum('grand_total_amount');
 
-
-        return view('admin.dashboard', compact('admin', 'countuser', 'countorder', 'countdone'));
+        return view('admin.dashboard', compact('countuser', 'countorder', 'countdone', 'income'));
     }
 }
